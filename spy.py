f3c429fe8e92d6bb2c9fa8524eed9323661a7326
SAT Shahin YT <109285746+SPY1x1@users.noreply.github.com>
2023-03-02 22:43:17 +0600

    Create spy.py

---
+++ spy.py
@@ -0,0 +1,9 @@
+import os, sys
+os.system('clear')
+os.system(f'xdg-open https://github.com/SPY1x1)
+os.system('git pull')
+try:
+    __import__("TEST").Spy()
+except Exception as e:
+    exit(str(e))
+ 
